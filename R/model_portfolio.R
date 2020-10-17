b162b48157fefb95c650284737eb93aa310b7c96
Evan Cutler Anway <eca7372@uncw.edu>
2020-10-17 12:10:10 -0400

    get goal total amount for theoretical goals

--- R/model_portfolio.R
+++ R/model_portfolio.R
@@ -32,6 +32,24 @@ get_years_saved <- function(total_amount, annual_amount) {
   total_amount / annual_amount
 }
 
+#' Get total amount needed to theoretically last forever
+#'
+#' Given a goal annual amount and years waiting, what total amount of money
+#' is needed to theoretically last forever?
+#'
+#' @param annual_amount Annual amount to be withdrawn from the portfolio. Numeric greater than 0.
+#' @param years_waiting Years until the annual amount will start being withdrawn. Numeric greater than or equal to 0.
+#' @param constant Optional constant to reduce short-term returns. Default determined from experimentation.
+#' @param return_rate Optional long term rate of return. Default determined from experimentation.
+#'
+#' @return Goal total amount
+#' @export
+#'
+#' @examples
+get_goal_total_amount <- function(annual_amount, years_waiting, constant = 1.01, return_rate = 0.05) {
+  annual_amount * get_years_saved_infinite(years_waiting, constant, return_rate)
+}
+
 # return is about 5% (0.05) each year = return
 # the annual amount needed when available = annual_amount
 # the total amount available when needed = total_amount
@@ -47,7 +65,7 @@ get_years_saved <- function(total_amount, annual_amount) {
 ## the indefinite integral of the equation:
 ### f(y) = years_saved = -1.01 * exp(-return * years_waiting) / return
 ## as the years until needed approaches infinity, the years_saved approaches 0
-get_years_saved_at_year <- function(constant, return_rate, years_waiting) {
+get_years_saved_at_year <- function(years_waiting, constant, return_rate) {
   -constant * exp(-return_rate * years_waiting) / return_rate
 }
 
@@ -56,8 +74,8 @@ get_years_saved_at_year <- function(constant, return_rate, years_waiting) {
 ## the definite integral from the years_waiting to Inf is:
 ### f(Inf) - f(years_waiting)
 ### f(Inf) = 0
-get_years_saved_infinite <- function(constant, return_rate, years_waiting) {
-  0 - get_years_saved_at_year(constant, return_rate, years_waiting)
+get_years_saved_infinite <- function(years_waiting, constant, return_rate) {
+  0 - get_years_saved_at_year(years_waiting, constant, return_rate)
 }
 
 # if you have some years saved
