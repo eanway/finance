285290d9d4ec0d4969d5166de575f8bc71731bde
Evan Cutler Anway <eca7372@uncw.edu>
2021-02-21 12:28:21 -0500

    move income functions to separate file

---
+++ R/income.R
@@ -0,0 +1,38 @@
+#' Income percentile
+#'
+#' Estimates income percentile given current age and income.
+#'
+#' @return Estimated income percentile
+#' @export
+#'
+#' @examples
+#' get_income_percentile(25, 30000)
+get_income_percentile <- function(age, income) {
+  predict(
+    smooth_percent_by_income_age, data.frame(age = age, income = income)
+  ) %>%
+    unname()
+}
+
+#' Get future income trajectory
+#'
+#' Estimates future income until retirement from current age and income.
+#'
+#' @param current_age Current age
+#' @param current_income Current income
+#'
+#' @return Vector of future incomes
+#' @export
+#'
+#' @examples
+get_future_income <- function(current_age, current_income, retirement_age = 70) {
+  income_pct <- get_income_percentile(current_age, current_income)
+
+  vec_age_to_retirement <- current_age:retirement_age
+
+  predict(
+    smooth_income_by_percent_age,
+    data.frame(age = vec_age_to_retirement, percent = income_pct)
+  ) %>%
+    setNames(vec_age_to_retirement)
+}
--- R/tax_efficient_deferrals.R
+++ R/tax_efficient_deferrals.R
@@ -1,57 +1,16 @@
-#' Income percentile
-#'
-#' Estimates income percentile given current age and income.
-#'
-#' @return Estimated income percentile
-#' @export
-#'
-#' @examples
-#' get_income_percentile(25, 30000)
-get_income_percentile <- function(age, income) {
-  predict(
-    smooth_percent_by_income_age, data.frame(age = age, income = income)
-  ) %>%
-    unname()
-}
-
-#' Get future income trajectory
-#'
-#' Estimates future income until retirement from current age and income.
-#'
-#' @param current_age Current age
-#' @param current_income Current income
-#'
-#' @return Vector of future incomes
-#' @export
-#'
-#' @examples
-get_future_income <- function(current_age, current_income, retirement_age = 70) {
-  income_pct <- get_income_percentile(current_age, current_income)
-
-  vec_age_to_retirement <- current_age:retirement_age
-
-  predict(
-    smooth_income_by_percent_age,
-    data.frame(age = vec_age_to_retirement, percent = income_pct)
-  ) %>%
-    setNames(vec_age_to_retirement)
-}
-
 #' Tax efficient deferrals
 #'
-#' @param income Current income
-#' @param age Current age
-#' @param income_trajectories Table of income trajectories
+#' @param current_age Current age
 #' @param tax_brackets Table of tax brackets
+#' @param current_savings_tax_deferred Current amount of tax-deferred savings
+#' @param future_income Vector of future income until retirement
 #'
 #' @return
 #' @export
 #'
 #' @examples
 tax_efficient_deferrals <- function(
-  current_age, future_income,
-  income_trajectories = income_trajectories_2021,
-  tax_brackets = tax_brackets_2021
+  current_age, tax_brackets, current_savings_tax_deferred, future_income
 ) {
 
 }
